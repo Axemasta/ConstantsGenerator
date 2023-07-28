65ffa5ef5c105487bf0ad56283ae4bdc78112236
Alex <33064621+Axemasta@users.noreply.github.com>
2023-07-28 20:36:32 +0100

    Delete DebugApp
    This wasn't needed anymore and makes everything cleaner

--- src/DebugApp/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DebugApp;
-
-public static class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("This project is used to debug our source generators");
-    }
-}
