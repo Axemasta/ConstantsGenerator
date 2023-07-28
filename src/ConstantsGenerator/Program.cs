b45339e1f011a111fe56f4b34c164a5cdc82d4ac
Alex <33064621+Axemasta@users.noreply.github.com>
2023-07-28 15:36:30 +0100

    Fix Hello World Generator On Windows
    I hadn't set the generator up properly and it didnt run on windows, pretty funny it works on mac

--- src/ConstantsGenerator/Program.cs
+++ src/ConstantsGenerator/Program.cs
@@ -4,7 +4,12 @@ partial class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Starting...");
+
         HelloFrom("Generated Code");
+
+        Console.WriteLine("Press any key to exit...");
+        _ = Console.ReadKey();
     }
 
     static partial void HelloFrom(string name);
